23c5c12a6e237bcd81be48025b883afdc64d0a1b
Brian Pan <b97705021@ntu.edu.tw>
2015-11-14 12:20:18 +0800

    Header finish

--- react/src/components/Header/Header.js
+++ react/src/components/Header/Header.js
@@ -7,11 +7,29 @@ import Link from '../Link';
 import Navigation from '../Navigation';
 
 @withStyles(styles)
-class Header extends Component {
 
+class Header extends Component {
   render() {
+    const divStyle = {
+      backgroundColor: 'white'
+    };
     return (
       <div className="Header">
+        <header className="mdl-layout__header">
+          <div className="mdl-layout__header-row" style = {divStyle}>
+            <img className="Header-brand" src={require('./redcross.png')} />
+          </div>
+          <div className="mdl-layout__header-row">
+            <span className="mdl-layout-title">Sophia Project</span>
+            <div className="mdl-layout-spacer"></div>
+            <nav className="mdl-navigation mdl-layout--large-screen-only">
+              <a className="mdl-navigation__link" href="">我要捐款</a>
+              <a className="mdl-navigation__link" href="">我要捐血</a>
+              <a className="mdl-navigation__link" href="">怎麼幫助</a>
+              <a className="mdl-navigation__link" href="">取得協助</a>
+            </nav>
+          </div>
+        </header>
         <div className="Header-container">
           <a className="Header-brand" href="/" onClick={Link.handleClick}>
             <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
